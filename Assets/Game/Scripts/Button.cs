2f9c715a5b9d824ba550b3b72bf3b2d5d8b3a9a3
Denny <dennymarcoalbert@gmail.com>
2020-11-16 21:30:37 +0700

    Adding AsyncAwait Plugin Transition

---
+++ Assets/Game/Scripts/Button.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Game.UI.Button
+{
+    [RequireComponent(typeof(Image))]
+	public class Button : MonoBehaviour, IPointerDownHandler, IPointerExitHandler, IPointerUpHandler
+	{
+		public event Action ButtonDown;
+		public event Action ButtonUp;
+		public event Action ButtonExit;
+
+		private bool _active = true;
+		private bool _pressed = false;
+
+		void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
+		{
+			if (!_active) return;
+
+			_pressed = true;
+			ButtonDown?.Invoke();
+		}
+
+		void IPointerUpHandler.OnPointerUp(PointerEventData eventData)
+		{
+			if (!_active || !_pressed) return;
+			if (eventData.dragging) return;
+
+			_pressed = false;
+			ButtonUp?.Invoke();
+		}
+
+		void IPointerExitHandler.OnPointerExit(PointerEventData eventData)
+		{
+			if (!_active) return;
+
+			_pressed = false;
+			ButtonExit?.Invoke();
+		}
+
+
+		protected virtual void OnDestroy()
+		{
+			ButtonDown = null;
+			ButtonUp = null;
+			ButtonExit = null;
+		}
+	}
+}
---
+++ Assets/Game/Scripts/SceneManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Game.Scene
+{
+    public class SceneManager : MonoBehaviour
+    {
+        private static readonly int FadeOut = Animator.StringToHash("FadeOut");
+        private static readonly int FadeIn = Animator.StringToHash("FadeIn");
+        
+        private Animator animator;
+
+        private async void Awake()
+        {
+            animator = GetComponent<Animator>();
+
+            await Crossfade(FadeIn);
+        }
+
+        public async void ChangeScene(string scene)
+        {
+            await Crossfade(FadeOut);
+
+            UnityEngine.SceneManagement.SceneManager.LoadScene(scene);
+        }
+
+        private async Task Crossfade(int animation)
+        {
+            Time.timeScale = 1;
+            animator.SetTrigger(animation);
+
+            await new WaitForSeconds(1);
+        }
+    }
+}
