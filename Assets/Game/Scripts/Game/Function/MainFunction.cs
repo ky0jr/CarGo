9049ecfe3f60c6dfa790dbd7f68ce6b26b1e8d41
Denny <dennymarcoalbert@gmail.com>
2021-03-14 21:15:36 +0700

    Max Command to 15 Add Environment Fix Lighting

--- Assets/Game/Scripts/Game/Function/MainFunction.cs
+++ Assets/Game/Scripts/Game/Function/MainFunction.cs
@@ -2,7 +2,7 @@
 {
     public class MainFunction: Function
     {
-        protected override int MaxCommand  => 16;
+        protected override int MaxCommand  => 15;
         public override FunctionType FunctionType => FunctionType.Main;
     }
 }
